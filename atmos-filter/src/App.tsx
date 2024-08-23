d30d8125d595ec811ae85a2dcc15555075dd4ddd
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-08-23 17:16:45 +0545

    first commit

---
+++ atmos-filter/src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import { Home } from "lucide-react";
+import Example from "./components/Home";
+
+export default function App() {
+  return (
+    // <Router>
+    //   <div className="flex h-screen">
+    //     <Sidebar />
+    //   </div>
+    // </Router>
+    <Example/>
+  );
+}
