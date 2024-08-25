208d56c1cb92839a47f6b51c8d770802ffb14545
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-08-25 12:53:36 +0545

    Top seller component

--- atmos-filter/src/App.tsx
+++ atmos-filter/src/App.tsx
@@ -2,6 +2,8 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import MainContent from "./components/MainContent";
 import ProductPage from "./components/ProductPage";
+import TopSellers from "./components/TopSellers";
+import PopularBlogs from "./components/PopularBlogs";
 
 export default function App() {
   return (
@@ -14,6 +16,12 @@ export default function App() {
             <Route path="/" element={<MainContent />} />
             <Route path="/product/:id" element={<ProductPage />} />
           </Routes>
+
+          <div>
+            <TopSellers/>
+            <PopularBlogs/>
+          </div>
+          
         </div>
       </div>
     </Router>
